0fd7d784867a277b39aee96649422d86a7a38a1d
Delcio Brito <delciobrito82@gmail.com>
2025-06-21 19:38:56 -0300

    Test final 2

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import createApp from "./app";
 
 const app = createApp();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
